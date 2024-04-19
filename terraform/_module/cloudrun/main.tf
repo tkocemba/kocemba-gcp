d362f4a60eda3524c85947784255ce0ba16a54c4
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-04-19 21:54:03 +0200

    Enabling modules

---
+++ terraform/_module/cloudrun/main.tf
@@ -0,0 +1,37 @@
+resource "google_cloud_run_service" "default" {
+  count = var.enable ? 1:0
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  count = var.enable ? 1:0
+  location    = google_cloud_run_service.default[0].location
+  project     = google_cloud_run_service.default[0].project
+  service     = google_cloud_run_service.default[0].name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
