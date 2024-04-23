490f16e066bb019487ab1c8c2ea9d3b6c894f7f1
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-04-23 13:34:13 +0200

    custom workflow

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -5,20 +5,4 @@ module "cloudrun" {
 }
 //
 
-resource null_resource aaa {
-}
-
-resource null_resource aaab {
-}
-
-resource null_resource aaab55 {
-}
-
-resource null_resource aaab556 {
-}
-
-resource null_resource user2 {
-}
-
-resource null_resource user33 {
-}
+resource "null_resource" "dddddd" {}
