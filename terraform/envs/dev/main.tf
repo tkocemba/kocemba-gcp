9bedd5bba58e519299d35058c6f2a89b555e46b1
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-05-13 11:53:49 +0200

    feature with bug

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -15,3 +15,23 @@ resource "null_resource" "prdevfeature4" {}
 resource "null_resource" "prdevfeature5" {}
 
 resource "null_resource" "devqafeature1" {}
+
+locals {
+  command = "set -u; echo $${envv}"
+}
+
+resource "null_resource" "prdevfeaturebug" {
+
+  provisioner "local-exec" {
+    command = local.command
+    environment = {
+      env = "tomek"
+    }
+  }
+
+  triggers = {
+    command_sha1=sha1(local.command)
+  }
+}
+
+
