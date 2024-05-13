dac8fe5a56ad6597ef9dff0c5ef1d7d65f738c05
tkocemba <tomasz.kocemba@gmail.com>
2024-05-13 21:46:27 +0200

    qa feature2 (#45)

--- terraform/envs/qa/main.tf
+++ terraform/envs/qa/main.tf
@@ -7,3 +7,4 @@ module "cloudrun" {
 resource "null_resource" "prqa1" {}
 resource "null_resource" "prfeatureqa" {}
 resource "null_resource" "featureqa1" {}
+resource "null_resource" "featureqa2" {}
