0b627eef8df379d1e6d20197fd8d060f4e82dd6d
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-05-13 22:19:22 +0200

    devqafeature

--- terraform/envs/qa/main.tf
+++ terraform/envs/qa/main.tf
@@ -9,3 +9,4 @@ resource "null_resource" "prfeatureqa" {}
 resource "null_resource" "featureqa1" {}
 resource "null_resource" "featureqa2" {}
 resource "null_resource" "devqafeature1" {}
+resource "null_resource" "devqafeature2" {}
