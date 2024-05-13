f03a60b37f0c09d9eb88a9feb276480fa902931f
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-05-13 10:53:34 +0200

    removed, custom

--- terraform/envs/prd/main.tf
+++ terraform/envs/prd/main.tf
@@ -3,6 +3,4 @@
 module "cloudrun" {
   source = "../../modules/cloudrun"
 }
-resource "null_resource" "a" {}
-resource "null_resource" "b" {}
-resource "null_resource" "c" {}
+resource "null_resource" "pr1dev" {}
