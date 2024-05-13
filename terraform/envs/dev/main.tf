e83a7e90022b9268814b8acadca33eaf96cd02ac
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-05-13 11:41:43 +0200

    feature4 on dev

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -10,6 +10,7 @@ resource "null_resource" "pr1dev1" {}
 resource "null_resource" "prdev2" {}
 resource "null_resource" "prdevfeature1" {}
 resource "null_resource" "prdevfeature2" {}
+resource "null_resource" "prdevfeature4" {}
 
 
 
