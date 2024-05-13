1f9456678b6247aa523644527369ade2512891b6
tkocemba <tomasz.kocemba@gmail.com>
2024-05-13 22:40:52 +0200

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -12,8 +12,7 @@ resource "null_resource" "prdevfeature1" {}
 resource "null_resource" "prdevfeature2" {}
 resource "null_resource" "prdevfeature3" {}
 resource "null_resource" "prdevfeature4" {}
-resource "null_resource" "prdevfeature5" {
-  
+resource "null_resource" "prdevfeature5" {  
 
 resource "null_resource" "devqafeature1" {}
 resource "null_resource" "devqafeature2" {}
