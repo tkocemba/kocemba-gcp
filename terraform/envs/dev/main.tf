fcb29a6cebf1516bdee65f22512952faf03a0501
tkocemba <tomasz.kocemba@gmail.com>
2024-05-13 22:35:47 +0200

    devfeature5 (#46)

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -12,4 +12,5 @@ resource "null_resource" "prdevfeature1" {}
 resource "null_resource" "prdevfeature2" {}
 resource "null_resource" "prdevfeature3" {}
 resource "null_resource" "prdevfeature4" {}
+resource "null_resource" "prdevfeature5" {}
 resource "null_resource" "devqafeature1" {}
