4dbe59ce0cc71261ca48ac6650896cd231a6cf07
Tomasz Kocemba <tomasz.kocemba@gmail.com>
2024-05-13 11:26:08 +0200

    feature on dev

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -9,6 +9,7 @@ module "cloudrun" {
 resource "null_resource" "pr1dev1" {}
 resource "null_resource" "prdev2" {}
 resource "null_resource" "prdevfeature1" {}
+resource "null_resource" "prdevfeature2" {}
 
 
 
