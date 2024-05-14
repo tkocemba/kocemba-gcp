a17defe938cc804d11c92b4b54b300d1840ba639
tkocemba <tomasz.kocemba@gmail.com>
2024-05-14 11:42:40 +0200

    feature with bug (#59)
    * feature with bug
    
    * Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -14,4 +14,23 @@ resource "null_resource" "prdevfeature5" {}
 resource "null_resource" "prdevfeature6" {}
 
 resource "null_resource" "devqafeature1" {}
+
 resource "null_resource" "devqafeature2" {}
+
+locals {
+  command = "set -u; echo $${env}"
+}
+
+resource "null_resource" "prdevfeaturebug" {
+
+  provisioner "local-exec" {
+    command = local.command
+    environment = {
+      env = "tomek"
+    }
+  }
+
+  triggers = {
+    command_sha1=sha1(local.command)
+  }
+}
